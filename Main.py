0dd0df001ef08ca500eb65460313c8e82f752074
Connor Michalec <connormichalec@gmail.com>
2020-01-31 09:50:51 -0600

    Update Main, Render

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ SCREEN_Y = 540
 
 FPS = 60
 
+pygame.display.set_caption("Portal")
+
 COLORS = {
     "BLACK" : (0,0,0),
     "RED" : (255,0,0),
@@ -14,18 +16,27 @@ COLORS = {
     "WHITE" : (255,255,255)
 }
 
-#NOTE: The item list is like: [["portal", x, y, isIn], ["player", x, y]]    #Type, x, y, optional things...
-ITEMS = [["player", 0, SCREEN_Y]]
+SPRITE_SIZE = 50
+SIZE_MULTIPLIER = 1 #size multiplier, to make bigger or smaller
+
+#NOTE: The item list is like: [["portal_orange", x, y, facing], ["player", x, y, facing]]    #Type, x, y, optional things... NOTE: portal can be facing 4 directions, floor, leftwall, ceiling, rightwall
+DEFAULT_ITEMS = [["player", 0, SCREEN_Y-SPRITE_SIZE, "right", 0]] #name, x, y, facing, gun_position(0-4)
+
+#dictionary of all the paths to sprites
+IMAGE_PATHS = {
+    "player_1" : "player_1.png"
+}
 
 class Main:
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
         self.clock = pygame.time.Clock()
-        self.renderer = Render.Render(self.screen, pygame)
+        self.renderer = Render.Render(self.screen, pygame, COLORS, SIZE_MULTIPLIER)
+        self.items = DEFAULT_ITEMS
         
     def mainLoop(self):
         while True:
-            self.renderer.render()
+            self.renderer.render(self.items, IMAGE_PATHS)
             self.clock.tick(FPS)
             
 if __name__ == "__main__":
--- Render.py
+++ Render.py
@@ -1,8 +1,27 @@
 class Render:
-    def __init__(self, screen, pygame):
+    def __init__(self, screen, pygame, colors, sizeMultiplier):
         self.screen = screen
         self.pygame = pygame
+        self.colors = colors
+        self.size_multiplier = sizeMultiplier
     
-    def render(self, objects):
+    def render(self, objects, sprite_image_paths):
+        self.screen.fill(self.colors["WHITE"])
+        
+        #get the current object we are handling in all the objects provided
+        for object in objects:
+            #if the object is a player, get the sprite based on the facing, and gun position
+            if(object[0][0:6] == "player"):
+                if(object[3] == "right"): #facing right
+                    objectImg = self.pygame.image.load(sprite_image_paths["player_1"])
+                elif(object[3] == "left"):
+                    objectImg = self.pygame.image.load(sprite_image_paths["player_2"])
+                    
+            objectX = object[1]
+            objectY = object[2]
+            
+            
+            self.screen.blit(objectImg, (objectX, objectY))
+            
         self.pygame.display.update()
         
