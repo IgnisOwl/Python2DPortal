e0e927a13fd382927df207f633b4f0e7a71b1f70
Ryan To <ryankto1@gmail.com>
2020-01-31 16:45:20 -0600

    Portal Movements

--- moveTick.py
+++ moveTick.py
@@ -1,15 +1,15 @@
 import pygame
 
 #Decides new positional data depending on User Input
-def moveTick(pos,velo):
-    if event.key == pygame.KEYDOWN
+def moveTick(pos,velo,height,walls):
+    if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_a or event.key == pygame.K_LEFT:
             velo[0]=-20
             pos[0]+=velo[0]
         if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
             velo[0]=20
             pos[0]+=velo[0]
-        if onGround():
+        if onGround(height,walls):
             if event.key == pygame.K_w or event.key == pygame.K_SPACE or event.key == pygame.K_UP:
                 velo[1]=25
                 pos[1]+=velo[1]
@@ -18,12 +18,16 @@ def moveTick(pos,velo):
             if velo[1]<-40:
                 velo[1]=-40
             pos[1]+=velo[1]
-    elif event.key == pygame.KEYUP:
+    elif event.type == pygame.KEYUP:
         if event.key == pygame.K_a or event.key == pygame.K_LEFT:
             velo[0]=0
         if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
             velo[0]=0
     return pos,velo
 
-def onGround():
-    return true
+def onGround(height,walls):
+    w=False
+    for a in walls:
+        if a[2]==(heigth+pos[1])
+            w=True
+    return w
