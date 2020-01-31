8756732eaae7f0f88c2e158d5abb7da3546ff519
Connor Michalec <connormichalec@gmail.com>
2020-01-31 10:01:38 -0600

    Update Main, Render

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ SPRITE_SIZE = 50
 SIZE_MULTIPLIER = 1 #size multiplier, to make bigger or smaller
 
 #NOTE: The item list is like: [["portal_orange", x, y, facing], ["player", x, y, facing]]    #Type, x, y, optional things... NOTE: portal can be facing 4 directions, floor, leftwall, ceiling, rightwall
-DEFAULT_ITEMS = [["player", 0, SCREEN_Y-SPRITE_SIZE, "right", 0]] #name, x, y, facing, gun_position(0-4)
+DEFAULT_ITEMS = [["player", 0, SCREEN_Y-(SPRITE_SIZE*SIZE_MULTIPLIER), "right", 0]] #name, x, y, facing, gun_position(0-4)
 
 #dictionary of all the paths to sprites
 IMAGE_PATHS = {
@@ -31,7 +31,7 @@ class Main:
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
         self.clock = pygame.time.Clock()
-        self.renderer = Render.Render(self.screen, pygame, COLORS, SIZE_MULTIPLIER)
+        self.renderer = Render.Render(self.screen, pygame, COLORS, SPRITE_SIZE, SIZE_MULTIPLIER)
         self.items = DEFAULT_ITEMS
         
     def mainLoop(self):
--- Render.py
+++ Render.py
@@ -1,9 +1,10 @@
 class Render:
-    def __init__(self, screen, pygame, colors, sizeMultiplier):
+    def __init__(self, screen, pygame, colors, spriteSize, sizeMultiplier):
         self.screen = screen
         self.pygame = pygame
         self.colors = colors
         self.size_multiplier = sizeMultiplier
+        self.sprite_size = spriteSize
     
     def render(self, objects, sprite_image_paths):
         self.screen.fill(self.colors["WHITE"])
@@ -20,7 +21,7 @@ class Render:
             objectX = object[1]
             objectY = object[2]
             
-            
+            objectImg = self.pygame.transform.scale(objectImg, (self.sprite_size * self.size_multiplier, self.sprite_size * self.size_multiplier))
             self.screen.blit(objectImg, (objectX, objectY))
             
         self.pygame.display.update()
