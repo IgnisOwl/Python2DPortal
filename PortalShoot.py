06e112dd4b73f95c6a240dec3c52af011b226b0d
Ryan To <ryankto1@gmail.com>
2020-01-31 18:23:20 -0600

    Portal Movements

--- PortalShoot.py
+++ PortalShoot.py
@@ -1,4 +1,9 @@
 import pygame
-def shootPortal(mX,mY):
+def shootPortal(mX,mY,pos,wall):
     bool1,bool2,bool3=pygame.mouse.get_pressed()
-    if
+    if bool1:        
+
+    if bool2:
+
+def detectWall(x,y,wall)
+    for
