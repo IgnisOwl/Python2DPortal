c105a815ea9dc4a27bc71471e10f74ce8158d012
Ryan To <ryankto1@gmail.com>
2020-01-31 18:50:23 -0600

    Portal Movements

--- PortalShoot.py
+++ PortalShoot.py
@@ -1,9 +1,30 @@
+import math
 import pygame
-def shootPortal(mX,mY,pos,wall):
+def shootPortal(mX,mY,pos,wall,tiles):
     bool1,bool2,bool3=pygame.mouse.get_pressed()
-    if bool1:        
-
-    if bool2:
-
-def detectWall(x,y,wall)
-    for
+    if bool1:#orange        
+        for x in tiles:
+            if x[0]=="portal_orange":
+                try:
+                    tiles.remove(index(x))#RETURN TILES
+                except:
+                    pass
+            
+            tiles.append(["portal_orange",x,y])
+    if bool2:#bue
+        pass
+def detectWall(x,y,xPos,yPos,wall):
+    if xPos:
+        x=ceiling(x)
+    else:
+        x=floor(x)
+    if yPos:
+        y=ceiling(y)
+    else:
+        y=floor(y)
+    for w in wall:
+        if w[1]==x:
+            return true,'x'
+        elif w[2]==y:
+            return true,'y'
+    return false,'z'
